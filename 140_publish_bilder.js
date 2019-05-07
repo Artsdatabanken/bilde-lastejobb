bb944b18d4e4f7bc756979ea70efaafaf526e7d0
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-07 19:48:03 +0200

    Deploy bilder på ny struktur

--- 140_publish_bilder.js
+++ 140_publish_bilder.js
@@ -1,28 +1,34 @@
-const io = require("./lib/io");
+const { io } = require("lastejobb");
 const fs = require("fs");
 const path = require("path");
 const config = require("./config");
 
+const imagePath = config.imagePath.processed + "/";
+const widths = [40, 408, 950];
+
 var data = io.readJson(config.datakilde.metabase);
 
-const imagePath = config.imagePath.processed + "/";
-const widths = [408, 950];
+deployFrom("logo");
+deployFrom("foto");
+deployFrom("banner");
 
-Object.keys(data).forEach(kode => {
-  const node = data[kode];
-  const foto = node.foto;
-  widths.forEach(width => {
-    deploy("png", node, width, foto);
-    deploy("jpg", node, width, foto);
+function deployFrom(subdir) {
+  Object.keys(data).forEach(kode => {
+    const node = data[kode];
+    const foto = node.foto;
+    widths.forEach(width => {
+      const srcPath = path.join(imagePath, subdir, width.toString());
+      deploy(subdir, srcPath, "png", node, width, foto);
+      deploy(subdir, srcPath, "jpg", node, width, foto);
+    });
   });
-});
+}
 
-function deploy(ext, { kode, url }, width, foto) {
-  const fn = path.join(imagePath + width, kode + "." + ext);
+function deploy(subdir, srcPath, ext, { kode, url }, width, foto) {
+  const fn = path.join(srcPath, kode + "." + ext);
   if (!fs.existsSync(fn)) return;
-  const destFn = `forside_${width}.${ext}`;
-  if (kode === "AO") debugger;
-  if (foto.forside && foto.forside.url.indexOf(destFn) >= 0) return;
-  if (foto.banner && foto.banner.url.indexOf(destFn) >= 0) return;
+  const destFn = `${subdir}_${width}.${ext}`;
+  if (foto[subdir] && foto[subdir].url.indexOf(destFn) >= 0) return;
+  //  if (foto.banner && foto.banner.url.indexOf(destFn) >= 0) return;
   console.log(`scp "${fn}" "grunnkart@hydra:~/tilesdata/${url}/${destFn}"`);
 }
