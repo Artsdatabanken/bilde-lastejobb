5e1a11ad10f6b99c6cfaa570058fa0e656d2f152
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-07 19:47:12 +0200

    Opprydding i config-fil

--- config.js
+++ config.js
@@ -10,42 +10,17 @@ const config = {
     bbox: "../input/bbox.json",
     taxons: "../input/taxons.json",
     taxon_adb_photos: "../input/scientificNameId-fileId.txt",
-    metabase: "../data/metadata_med_undertyper.json"
+    metabase: "./data/metadata_med_undertyper.json"
   },
   datafil: {
-    nin_koder: "../data/nin_koder.json",
-    nin_variasjon: "../data/nin_variasjon.json",
-    nin_grunntyper: "../data/nin_grunntyper.json",
-    nin_hovedtyper: "../data/nin_hovedtyper.json",
-    nin_prosedyrekategori: "../data/nin_prosedyrekategori.json",
-    nin_definisjonsgrunnlag: "../data/nin_definisjonsgrunnlag.json",
-    nin_koder_importert: "../data/nin_koder_importert.json",
-    nin_variasjon_importert: "../data/nin_variasjon_importert.json",
     nin_liste: "../data/nin_liste.json",
     artsfoto: "../data/artsfoto.json",
     nin_foto: "../data/nin_foto.json",
-    bbox_30: "../data/bbox_30.json",
-    nin_hierarki_overrides: "../data/nin_hierarki_overrides.json",
-    taxon_50: "../data/taxon_50.json",
-    kommune_60: "../data/kommune_60.json",
-    fylke_61: "../data/fylke_61.json",
-    nin_diagnostisk_art: "../data/nin_diagnostisk_art.json",
-    andre_koder: "../data/andre_koder.json",
-    organisasjon: "../data/organisasjon.json",
-    våpenskjold_meta: "../data/110_våpenskjold_meta.json",
-    dominant_farge: "../data/dominant_farge.json",
-    farger: "../data/farger.json",
-    flettet: "../data/flettet.json",
-    metabase: "../data/metabase.json",
-    kodetre: "../data/kodetre.json",
-    koder_med_forfedre: "../data/koder_med_forfedre.json",
-    metagammel: "../data/metagammel.json"
+    taxon_50: "../data/taxon_50.json"
   },
   imagePath: {
     source: "../image/source",
-    processed: "../image/processed",
-    custom_avatar: "../input/avatar",
-    custom_omslag: "../input/omslag"
+    processed: "../image/processed"
   },
   prefix: {
     natursystem: "NA_",
