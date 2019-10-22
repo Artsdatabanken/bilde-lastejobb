41c56ef0d61ca1ab5c4235c8a8c58aaa80f3b83e
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-22 10:49:47 +0200

    experiment 4

--- 120_konverter_bilder.js
+++ 120_konverter_bilder.js
@@ -12,17 +12,14 @@ function convertSync(kildesti, målsti, format, width, height = "", bildetype) {
   const ikkeCrop = erLogo;
   const faded = erLogo && width < 100;
   const erBanner = width > 1.5 * height;
-  const args = [
+  var args = [
     "-auto-orient",
-    "-resize",
-    width + "x" + height + (ikkeCrop ? "" : "^"),
     "-gravity",
     erBanner ? "west" : "center",
     erBanner ? "-extent" : "",
     erBanner ? width + "x" + height : "",
     "+repage",
-    "-crop",
-    width + "x" + height + "+0+0",
+    //    "-crop", width + "x" + height + "+0+0",
     "-background",
     "transparent",
     "-transparent",
@@ -35,14 +32,27 @@ function convertSync(kildesti, målsti, format, width, height = "", bildetype) {
     "A",
     "-evaluate",
     "multiply",
-    faded ? "0.6" : "1.0",
+    faded ? "0.65" : "1.0",
     //    '-verbose',
-    "-debug",
-    "user",
+    //"-debug", "user",
     "-path",
-    målsti,
-    kildesti
+    målsti
   ];
+  if (width === 40) {
+    args.push("-extent");
+    args.push("40x40+0+0");
+  }
+  if (width === 40) {
+    width = 30;
+    height = 30;
+  }
+
+  // mogrify -auto-orient -gravity center +repage -background transparent -transparent white -format png -density 600 -auto-level -path build/phylopic/40 -resize 30x30 -extent 30x30 -border 5x5 -bordercolor transparent -verbose data/phylopic/AR-123932.png && file build/phylopic/40/AR-123932.png && eog build/phylopic/40/AR-123932.png
+
+  args.push("-resize");
+  args.push(width + "x" + height + (ikkeCrop ? "" : "^"));
+  args.push(kildesti);
+
   console.log("mogrify " + args.join(" "));
   //  const r = spawnSync("mogrify", args);
   //  log.debug(r.output.toString());
